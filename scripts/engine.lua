c1f08a7404c39f1b6cbd8e072f8d8b7c4574cf2d
Qoalabear <512290+qoala@users.noreply.github.com>
2022-03-13 18:45:39 -0700

    Fix typoed self reference

--- scripts/engine.lua
+++ scripts/engine.lua
@@ -2,8 +2,8 @@
 local simengine = include( "sim/engine" )
 
 function simengine:uitr_resetAllUnitVision()
-	sim:getTags().uitr_oneCellVision = nil
-	sim:getTags().uitr_oneVision = nil
+	self:getTags().uitr_oneCellVision = nil
+	self:getTags().uitr_oneVision = nil
 
 	for unitID,unit in pairs( self:getAllUnits() ) do
 		unit:getTraits().uitr_hideVision = nil
