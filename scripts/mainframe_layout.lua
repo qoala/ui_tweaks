c8494c64432b03e545ad4039e5d2f92f7eef3992
Qoalabear <512290+qoala@users.noreply.github.com>
2022-10-30 01:43:26 -0700

    Treat wide widgets as rounded rectangles
    Still need to add a vector that includes horizontal center separation
    when they horizontally overlap

--- scripts/mainframe_layout.lua
+++ scripts/mainframe_layout.lua
@@ -12,20 +12,22 @@ local uitr_util = include( SCRIPT_PATHS.qed_uitr .. "/uitr_util" )
 
 local TUNING =
 {
-	-- Magnitude at which buttons/static regions push away at eachother (button_layout: 5)
-	repulseMagnitude = 5,
-	-- Inverse squared magnitude is capped at this minimum distance. (button_layout: 40)
-	repulseScaleCap = 0.4,
-	-- Repulsion is applied up to this amount of separation + bounding radii.
-	repulseMaxSep = 20,
 	-- Max iterations to figure out a layout placement. (button_layout: 10)
 	maxIters = 10,
 
-	-- Horizontal spacing between spacers (both static and wide widgets).
-	itemSpacing = 40,
+	-- Magnitude at which buttons/static regions push away at eachother (button_layout: 5)
+	repulseMagnitude = 5,
+	-- Inverse squared magnitude distance is capped at this times combined radii. (button_layout: 40)
+	repulseScaleCap = 0.4,
+	-- Repulsion is applied up to this amount of separation + bounding radii.
+	repulseMaxSep = 20,
 
-	-- Overlap radius of layout widgets/spacer.
+	-- Overlap radius of layout items. (button_layout: 42)
 	itemRadius = 21,
+	-- Distance between horizontal centers of the rounded-rectangle bounding box.
+	-- Approx. difference in width between daemonKnown(234x60px) and daemonUnknown(61x61px)
+	wideItemWidth = 170,
+
 	-- Overlap radius of the static bubble at the target circle for a layout item.
 	staticIceRadius = 3,
 	-- Overlap radius of the static bubble around fixed widgets, by skin name.
@@ -33,13 +35,7 @@ local TUNING =
 		["Activate"] = 31,
 		["Target"] = 21,
 	},
-	-- Additional horizontal spacers for wide fixed widgets, by skin name.
-	staticSpacerCount = {
-		["Activate"] = 6,
-	},
-	staticSpacerRadius = {
-		["Activate"] = 21,
-	},
+	-- Activate's label is scaled, but at 1920x1080 is approx. 384x32px.
 }
 
 -- Extra elements for layout widget-lists to make the layout entry wider.
@@ -75,11 +71,10 @@ function mainframe_layout:refreshTuningSettings()
 		self._tuning.repulseMaxSep = uitrSettings.mainframeLayoutMaxSeparation
 
 		self._tuning.itemRadius = uitrSettings.mainframeLayoutItemRadius
+
 		self._tuning.staticIceRadius = uitrSettings.mainframeLayoutStaticIceRadius
 		self._tuning.staticRadius["Activate"] = uitrSettings.mainframeLayoutStaticActivateRadius
 		self._tuning.staticRadius["Target"] = uitrSettings.mainframeLayoutStaticTargetRadius
-		self._tuning.staticSpacerCount["Activate"] = uitrSettings.mainframeLayoutStaticActivateTextWidth
-		self._tuning.staticSpacerRadius["Activate"] = uitrSettings.mainframeLayoutStaticActivateTextRadius
 
 		self._lastSettingsID = uitrSettings._tempID
 		self._dirty = true
@@ -103,7 +98,7 @@ function mainframe_layout.restoreWidgets( widgets )
 	end
 end
 
-function mainframe_layout:_refreshWidgets( screen, game, widgets )
+function mainframe_layout:_refreshDirtyWidgets( screen, game, widgets )
 	-- UITR: Populate the list of statics from static widgets each time we calculate.
 	self._statics = {}
 
@@ -125,15 +120,17 @@ function mainframe_layout:_refreshWidgets( screen, game, widgets )
 			-- UITR: other widgets should be drawn directly on their coordinates.
 			widget._layoutID = nil
 
-			if widget.getName and self._tuning.staticRadius[widget:getName()] then
-				-- UITR: Insert into the static forcing coordinates list to keep clear around this button.
+			if widget.getName then
 				local radius = self._tuning.staticRadius[widget:getName()]
-				table.insert(self._statics, {
-					worldx = widget.worldx,
-					worldy = widget.worldy,
-					worldz = widget.worldz,
-					radius = radius,
-				})
+				if radius and radius > 0 then
+					-- UITR: Insert into the static forcing coordinates list to keep clear around this button.
+					table.insert(self._statics, {
+						worldx = widget.worldx,
+						worldy = widget.worldy,
+						worldz = widget.worldz,
+						radius = radius,
+					})
+				end
 			end
 		end
 
@@ -145,35 +142,27 @@ function mainframe_layout:_refreshWidgets( screen, game, widgets )
 				-- UITR: Move to back.
 				screen:reorderWidget( leaderWidget, 1 )
 				leaderWidget.binder.line:appear( 0.5 )
+
 				layout = {
-					widgets = { widget },
+					widget = widget,
 					leaderWidget = leaderWidget
 				}
 				self._layout[ layoutID ] = layout
 			else
+				-- Mark layout as in use.
 				array.removeElement(oldIDs, layoutID)
+
+				layout.widget = widget
 			end
 
 			-- UITR: Keep a small area clear around the target circle for this item.
-			if self._tuning.staticIceRadius then
+			if self._tuning.staticIceRadius and self._tuning.staticIceRadius > 0 then
 				table.insert(self._statics, {
 					worldx = widget.worldx,
 					worldy = widget.worldy,
 					worldz = widget.layoutWorldz or widget.worldz,
 					radius = self._tuning.staticIceRadius,
 				})
-			end
-
-			-- UITR: Each layout entry only gets a single widget, unlike the meatspace button layout.
-			--       If the widget is extra-wide, add some dummy entries for the spacing calculations.
-			if widget.layoutWide then
-				 -- Layout treats each widget as 40px wide.
-				 -- program.daemonKnown.bg is 234px wide, including borders.
-				layout.widgets = { widget, LEFTMID_SPACER, MIDDLE_SPACER, RIGHTMID_SPACER, RIGHT_SPACER }
-				widget._layoutLeft = true
-			else
-				layout.widgets = { widget }
-				widget._layoutLeft = nil
 			end
 
 			-- UITR: We'll be drawing our own arm, thank you very much.
@@ -193,47 +182,59 @@ end
 function mainframe_layout:calculateLayout( screen, game, widgets )
 	if self._dirty then
 		-- UITR: Handle new item creation/destruction only when needed.
-		self:_refreshWidgets(screen, game, widgets)
+		self:_refreshDirtyWidgets(screen, game, widgets)
 		self._dirty = false
 	end
 
 	-- Initial positions of layout-positioned elements.
 	local layoutsByPosition = {}
 	for _, layout in pairs( self._layout ) do
-		local widget = layout.widgets[1]
+		local widget = layout.widget
 
 		local wx, wy = game:worldToWnd( widget.worldx, widget.worldy, widget.layoutWorldz or widget.worldz )
 		layout.startx, layout.starty = wx, wy
-		layout.posx, layout.posy = wx, wy
+		layout.posxMin, layout.posy = wx, wy
 
 		-- UITR: Instead of radiating the offset away from a center point on the selected agent,
 		--       Use a fixed offset as the base offset.
 		local ox, oy = -18, -59
-		local radius = mathutil.dist2d( 0, 0, ox, oy )
-		layout.posx, layout.posy = layout.posx + ox, layout.posy + oy
+		layout.posxMin, layout.posy = layout.posxMin + ox, layout.posy + oy
+
+		if widget.layoutWide then
+			-- Above is the left-most center, where the number is centered.
+			-- Calculate true center of the box and right-most center.
+			local width = self._tuning.wideItemWidth
+			layout.posxMid, layout.posxMax = layout.posxMin + width / 2, layout.posxMin + width
+			layout.posWidth = width
+		else
+			layout.posxMid, layout.posxMax = layout.posxMin, layout.posxMin
+			layout.posWidth = 0
+		end
 
 		-- UITR: Track layouts that start on the exact same coordinate.
-		local positionKey = tostring(layout.posx) .. "|" .. tostring(layout.posy)
+		local positionKey = tostring(layout.posxMid) .. "|" .. tostring(layout.posy)
 		if layoutsByPosition[positionKey] then
 			table.insert( layoutsByPosition[positionKey], layout )
 		else
 			layoutsByPosition[positionKey] = { layout }
 		end
 	end
-	-- UITR: Vary overlapping positions so that they are able to separate in the later calculations.
+	-- UITR: Vary identical positions so that they are able to separate in the later calculations.
 	--       Unlike base layout, only do this when there's an exact match, so that otherwise colinear items can remain colinear.
 	for _,layouts in pairs(layoutsByPosition) do
 		if #layouts > 1 then
-			for i, layout in ipairs(layouts) do
+			for i, l in ipairs(layouts) do
 				local dx, dy = 4 * math.cos( 2*math.pi * (i / #layouts) ), 4 * math.sin( 2*math.pi * (i / #layouts ))
-				layout.posx, layout.posy = layout.posx + dx, layout.posy + dy
+				l.posxMin, l.posy = l.posxMin + dx, l.posy + dy
+				l.posxMid, l.posxMax = l.posxMid + dx, l.posxMax + dx
 			end
 		end
 	end
 
 	-- Positions of static barrier elements.
 	for _,static in ipairs(self._statics) do
-		static.posx, static.posy = game:worldToWnd( static.worldx, static.worldy, static.worldz )
+		static.posxMin, static.posy = game:worldToWnd( static.worldx, static.worldy, static.worldz )
+		static.posxMid, static.posxMax = static.posxMin, static.posxMin
 	end
 
 	-- Now, shift positions to reduce overlap.
@@ -258,7 +259,7 @@ function mainframe_layout:setPosition( widget )
 	-- UITR: Follow the implementation of button_layout:setPosition to place the button at the selected offset.
 	--       Skip the steps that vary based on index within the layout element. Each element has only 1 widget.
 	local W, H = widget:getScreen():getResolution()
-	local x, y = widget:getScreen():wndToUI( layout.posx, layout.posy )
+	local x, y = widget:getScreen():wndToUI( layout.posxMin, layout.posy )
 	local startx, starty = widget:getScreen():wndToUI( layout.startx, layout.starty )
 
 	if hasArm(widget) then
@@ -274,7 +275,7 @@ function mainframe_layout:setPosition( widget )
 	layout.leaderWidget:setPosition( startx, starty )
 	local BUTTON_OFFSET_Y = ((36/2 - 4) / H)
 	local x1, y1 = x - startx, y - BUTTON_OFFSET_Y - starty
-	local wndDist = mathutil.dist2d( layout.startx, layout.starty, layout.posx, layout.posy - (36/2 - 4) )
+	local wndDist = mathutil.dist2d( layout.startx, layout.starty, layout.posxMin, layout.posy - (36/2 - 4) )
 	local t0 = 8 / wndDist -- target circle is 16x16
 	layout.leaderWidget.binder.line:setTarget( t0, x1, y1 )
 	return true
@@ -284,37 +285,46 @@ end
 -- Calculation tweaks
 -- ===
 
-function mainframe_layout:getCircle( layoutID, index )
-
-	local OFFSET_X = self._tuning.itemSpacing
-	local ll = self._layout[ layoutID ]
-	return ll.posx + OFFSET_X * (index-1), ll.posy, (ll.widgets[index].layoutRadius or self._tuning.itemRadius)
+-- Nearest separation between two horizontal lines defined by rounded-rectangle centers
+function mainframe_layout:_getSeparationDist( l0, l1 )
+	if l0.posxMin > l1.posxMax then
+		-- l0 fully right of l1: Compare endpoint centers.
+		return mathutil.dist2d( l0.posxMin, l0.posy, l1.posxMax, l1.posy )
+	elseif l1.posxMin > l0.posxMax then
+		-- l1 fully right of l0: Compare endpoint centers.
+		return mathutil.dist2d( l0.posxMax, l0.posy, l1.posxMin, l1.posy )
+	else
+		-- Overlapping: Compare y-only.
+		return math.abs(l0.posy - l1.posy)
+	end
 end
 
-function mainframe_layout:getStaticCircle( static )
-	return static.posx, static.posy, static.radius
+-- Nearest separation vector between two horizontal lines defined by rounded-rectangle centers
+function mainframe_layout:_getSeparationVector( l0, l1 )
+	if l0.posxMin > l1.posxMax then
+		-- l0 fully right of l1: Compare endpoint centers.
+		return l1.posxMax - l0.posxMin, l1.posy - l0.posy
+	elseif l1.posxMin > l0.posxMax then
+		-- l1 fully right of l0: Compare endpoint centers.
+		return l1.posxMin - l0.posxMax, l1.posy - l0.posy
+	else
+		-- Overlapping: Compare y-only.
+		return 0, l1.posy - l0.posy
+	end
 end
 
-function mainframe_layout:hasOverlaps( layout, statics )
-	local overlaps = 0
-	for layoutID, l in pairs( layout ) do
-		for i = 1, #l.widgets do
-			local x0, y0, r0 = self:getCircle( layoutID, i )
-			for w2, ll in pairs(layout) do
-				if w2 ~= layoutID then
-					for j = 1, #ll.widgets do
-						local x1, y1, r1 = self:getCircle( w2, j )
-						if mathutil.dist2d( x0, y0, x1, y1 ) <= r0+r1 then
-							return true
-						end
-					end
-				end
+function mainframe_layout:hasOverlaps( layouts, statics )
+	local itemRadius = self._tuning.itemRadius
+	local itemDiameter = itemRadius * 2
+	for id0, l0 in pairs( layouts ) do
+		for id1, l1 in pairs(layouts) do
+			if id0 ~= id1 and self:_getSeparationDist( l0, l1 ) <= itemDiameter then
+				return true
 			end
-			for i, static in pairs(statics) do
-				local x1, y1, r1 = self:getStaticCircle( static )
-				if mathutil.dist2d( x0, y0, x1, y1 ) <= r0+r1 then
-					return true
-				end
+		end
+		for _, l1 in ipairs(statics) do
+			if self:_getSeparationDist( l0, l1 ) <= itemRadius + l1.radius then
+				return true
 			end
 		end
 	end
@@ -322,55 +332,65 @@ function mainframe_layout:hasOverlaps( layout, statics )
 	return false
 end
 
-function mainframe_layout:updateForce( fx, fy, dx, dy, radius, left, mid, right )
+function mainframe_layout:updateForce( fx, fy, dx, dy, radius )
+	local SCALE_DIST = radius * self._tuning.repulseScaleCap
+	local MAX_DIST = radius + self._tuning.repulseMaxSep
 	local mag = self._tuning.repulseMagnitude
-	if mag then
-		local SCALE_DIST = radius * self._tuning.repulseScaleCap
-		local MAX_DIST = radius + self._tuning.repulseMaxSep
-		local d = math.sqrt( dx*dx + dy*dy )
-		if d < 1 then
-			return fx, fy
-		elseif d > MAX_DIST then
-			-- Far enough apart.
-			return fx, fy
-		else
-			mag = mag * math.min( 1, (SCALE_DIST * SCALE_DIST) / (d*d)) -- inverse sqr mag.
-			dx, dy = dx / d, dy / d
+	local d = math.sqrt( dx*dx + dy*dy )
+	if d > MAX_DIST then
+		-- Far enough apart.
+		return fx, fy
+	elseif d < 1 then
+		-- Too close.
+		if dx > 0 then
+			dx = 1
+		elseif dx < 0 then
+			dx = -1
 		end
-
-		if left and dx > 0 then
-			dx = 0
-		elseif right and dx < 0 then
-			dx = 0
-		elseif mid then
-			dx = dx * 3 / 4
+		if dy > 0 then
+			dy = 1
+		elseif dy < 0 then
+			dy = -1
 		end
-
-		fx, fy = fx + mag * dx, fy + mag * dy
+	else
+		mag = mag * math.min( 1, (SCALE_DIST * SCALE_DIST) / (d*d)) -- inverse sqr mag.
+		dx, dy = dx / d, dy / d
 	end
+
+	fx, fy = fx + mag * dx, fy + mag * dy
 	return fx, fy
 end
 
-function mainframe_layout:calculateForce( layoutID, layout, statics )
+function mainframe_layout:calculateForce( id0, l0, layouts, statics )
+	local itemRadius = self._tuning.itemRadius
+	local itemDiameter = itemRadius * 2
+
 	local fx, fy = 0, 0
-	local l = layout[ layoutID ]
-	for i = 1, #l.widgets do
-		local x0, y0, r0 = self:getCircle( layoutID, i )
-		for w2, ll in pairs(layout) do
-			if w2 ~= layoutID then
-				for j, widget in ipairs(ll.widgets) do
-					local x1, y1, r1 = self:getCircle( w2, j )
-					fx, fy = self:updateForce( fx, fy, x0 - x1, y0 - y1, r0 + r1, widget._layoutLeft, widget._layoutMid, widget._layoutRight )
-				end
-			end
+	for id1, l1 in pairs(layouts) do
+		if id0 ~= id1 then
+			local dx, dy = self:_getSeparationVector( l1, l0 )
+			fx, fy = self:updateForce( fx, fy, dx, dy, itemDiameter )
 		end
-		for i, ll in pairs(statics) do
-			local x1, y1, r1 = self:getStaticCircle( ll )
-			fx, fy = self:updateForce( fx, fy, x0 - x1, y0 - y1, r0 + r1, ll._layoutLeft, ll._layoutMid, ll._layoutRight )
-		end
+	end
+	for _, l1 in ipairs(statics) do
+		local dx, dy = self:_getSeparationVector( l1, l0 )
+		fx, fy = self:updateForce( fx, fy, dx, dy, itemRadius + l1.radius )
 	end
 
     return fx, fy
+end
+
+function mainframe_layout:doPass( layouts, statics )
+	for layoutID, l in pairs(layouts) do
+		-- Get the force on this widget.
+		l.fx, l.fy = self:calculateForce( layoutID, l, layouts, statics )
+	end
+
+	-- Apply forces
+	for _, l in pairs(layouts) do
+		l.posxMin, l.posy = l.posxMin + l.fx, l.posy + l.fy
+		l.posxMid, l.posxMax = l.posxMid + l.fx, l.posxMax + l.fx
+	end
 end
 
 return mainframe_layout
--- scripts/uitr_util.lua
+++ scripts/uitr_util.lua
@@ -87,19 +87,19 @@ local UITR_OPTIONS = {
 	{
 		id = "mainframeLayoutStaticIceRadius",
 		name = "  Static boundary for mainframe devices",
-		values={ 1,3,5,7,9,11,13,15,17,19,21,23,25 },
+		values={ 0, 1,3,5,7,9,11,13,15,17,19,21,23,25 },
 		value = 3,
 	},
 	{
 		id = "mainframeLayoutStaticActivateRadius",
 		name = "  Static boundary for activatables",
-		values={ 9,11,13,15,17,19,21,23,25, 31, 35, 41, 45, 51,  },
+		values={ 0, 9,11,13,15,17,19,21,23,25, 31, 35, 41, 45, 51,  },
 		value = 31,
 	},
 	{
 		id = "mainframeLayoutStaticActivateTextRadius",
 		name = "  Static boundary for activatable text",
-		values={ 9,11,13,15,17,19,21,23,25, 31, 35, 41, 45, 51,  },
+		values={ 0, 9,11,13,15,17,19,21,23,25, 31, 35, 41, 45, 51,  },
 		value = 21,
 	},
 	{
@@ -111,7 +111,7 @@ local UITR_OPTIONS = {
 	{
 		id = "mainframeLayoutStaticTargetRadius",
 		name = "  Static boundary for ability targets",
-		values={ 9,11,13,15,17,19,21,23,25 },
+		values={ 0, 9,11,13,15,17,19,21,23,25 },
 		value = 21
 	},
 
